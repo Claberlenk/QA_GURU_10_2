1bdc07af0486fe2dcd00f1dee99f35a7e86170b3
aleksei <styrostov94@gmail.com>
2024-01-15 19:17:59 -0300

    added project dependency

--- test_google.py
+++ test_google.py
@@ -1,6 +1,19 @@
 from selene import browser, be, have
+import pytest
 
+@pytest.fixture
+def browser_size():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield browser
+    browser.quit()
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+def test_google_request(browser_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_google_not_info(browser_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('-info').press_enter()
+    browser.element('[id=botstuff] [role=heading]').should(have.text('По запросу -info ничего не найдено.'))
